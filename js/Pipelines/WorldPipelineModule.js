6a647e70cab60ff92226ea2aa8a7dad3e2926d94
Alex Di Guida <alexandre.diguida@gmail.com>
2022-10-05 10:09:46 +0900

    Add vConsole

--- js/Pipelines/WorldPipelineModule.js
+++ js/Pipelines/WorldPipelineModule.js
@@ -1,11 +1,17 @@
 import Dummy from '../Experience/Dummy'
 import ParticleSystem from '../Experience/ParticleSystem'
 
+import VConsole from 'vconsole'
+
+const IS_CONSOLE = true
+
 export const initWorldPipelineModule = () => {
   let dummy
   let particleSystem
 
   const init = () => {
+    if (IS_CONSOLE) new VConsole({ theme: 'dark' })
+
     dummy = new Dummy()
     particleSystem = new ParticleSystem({ count: 1000 })
   }
